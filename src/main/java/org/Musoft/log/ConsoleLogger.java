f0d4c50273fa490a90f7567fb3f7b99fb2420301
Huseyin Erkek <Huseyin.erk.10@gmail.com>
2025-06-29 04:11:23 +0300

    feat: add ConsoleLog interface and its implementation

--- src/main/java/org/Musoft/log/ConsoleLogger.java
+++ src/main/java/org/Musoft/log/ConsoleLogger.java
@@ -1,4 +1,18 @@
 package org.Musoft.log;
 
-public class ConsoleLogger {
+public class ConsoleLogger implements ILogger {
+    @Override
+    public void info(String message) {
+        System.out.println("INFO " + message);
+    }
+
+    @Override
+    public void warn(String message) {
+        System.out.println("WARN " + message);
+    }
+
+    @Override
+    public void error(String message) {
+        System.out.println("ERROR " + message);
+    }
 }
--- src/main/java/org/Musoft/log/ILogger.java
+++ src/main/java/org/Musoft/log/ILogger.java
@@ -1,4 +1,7 @@
 package org.Musoft.log;
 
 public interface ILogger {
+    void info(String message);
+    void warn(String message);
+    void error(String message);
 }
