a6e744edd7f7a69eaf80ea8ecca0600233e820c5
Huseyin Erkek <Huseyin.erk.10@gmail.com>
2025-06-29 04:12:06 +0300

    feat: add ConsoleLog interface and its implementation

--- src/test/java/PassFailDeciderTest.java
+++ src/test/java/PassFailDeciderTest.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import org.Musoft.decider.PassFailDecider;
+import org.Musoft.log.ConsoleLogger;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PassFailDeciderTest {
+@Test
+public void testIsPassed() {
+    PassFailDecider decider = new PassFailDecider(new ConsoleLogger());
+
+    assertTrue(decider.isPassed(61.0));
+    assertFalse(decider.isPassed(60.0));
+    assertFalse(decider.isPassed(0));
 }
+}
+
