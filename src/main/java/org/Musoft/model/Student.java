1d10c09b1f9e87ee2229805530692186a89b4756
Huseyin Erkek <Huseyin.erk.10@gmail.com>
2025-06-29 04:10:49 +0300

    refactor: update Student entity

--- src/main/java/org/Musoft/model/Student.java
+++ src/main/java/org/Musoft/model/Student.java
@@ -1,4 +1,22 @@
 package org.Musoft.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
+   private String  name;
+   private List<Double> grades;
+
+    public Student(String name, List<Double> grades) {
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public List<Double> getGrades() {
+        return grades;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
