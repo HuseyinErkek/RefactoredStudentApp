fe5c35d6e6f0abaf848019994aa30c80d9ca6c55
Huseyin Erkek <Huseyin.erk.10@gmail.com>
2025-06-29 04:12:49 +0300

    feat: implement TopStudentFinder class

--- src/main/java/org/Musoft/service/TopStudentFinder.java
+++ src/main/java/org/Musoft/service/TopStudentFinder.java
@@ -1,4 +1,22 @@
 package org.Musoft.service;
 
-public class TopStudentFinder {
+import org.Musoft.model.Student;
+
+import java.util.AbstractMap;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+public class TopStudentFinder implements ITopStudentFinder {
+    @Override
+    public Optional<AbstractMap.SimpleEntry<Student, Double>> findTopStudent(List<Student> students, List<Double> averages) {
+        if (students == null || averages == null || students.size() != averages.size()) {
+            throw new IllegalArgumentException("Öğrenci ve ortalama listeleri boş veya uyumsuz.");
+        }
+
+        return IntStream.range(0, students.size())
+                .mapToObj(i -> new AbstractMap.SimpleEntry<>(students.get(i), averages.get(i)))
+                .max(Comparator.comparing(AbstractMap.SimpleEntry::getValue));
+    }
 }
